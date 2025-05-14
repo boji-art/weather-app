3ab414f6706c0888d53000e3253ffb6bde2f404e
boji-art <j.boloroo@yahoo.com>
2025-05-14 12:35:42 +0800

    city, temp

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,16 @@ import { Hailt } from "@/component/Hailt";
 import { Night } from "@/component/Night";
 
 import { Search } from "lucide-react";
-
-export default function Home() {
+import { useState } from "react";
+export default function Home(){
   return (
-  <div>
-    <Hailt/>
-   
-    <div className="flex w-full gap-0.5">
-      
-    <Day/>
-    <Night/>
-   
-     </div>
-     </div>
+    <div>
+      <Hailt />
+
+      <div className="flex w-full gap-0.5">
+        <Day />
+        <Night />
+      </div>
+    </div>
   );
 }
