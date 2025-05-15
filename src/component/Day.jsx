6edb7e3675eb007d02b827d2711ac69c8839e5a9
boji-art <j.boloroo@yahoo.com>
2025-05-15 12:41:34 +0800

    changes

--- src/component/Day.jsx
+++ src/component/Day.jsx
@@ -1,59 +1,27 @@
 import { Heart, House, MapPin, User } from "lucide-react";
-import { useState } from "react";
 
-export const Day = () => {
-  const [weather, setWeather] = useState({});
-  console.log(weather);
-  const [location, setLocation] = useState({});
-  console.log(location);
-
-  const cityUrl = `https://api.api-ninjas.com/v1/city?name=Tokyo`;
-
-  const getCity = async () => {
-    try {
-      const response = await fetch(cityUrl, {
-        headers: {
-          "X-APi-Key": process.env.NEXT_PUBLIC_CITY_API_KEY,
-        },
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const getWeather = async () => {
-    try {
-      const cityLocation = await getCity();
-      console.log(cityLocation);
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${cityLocation[0].latitude}&lon=${cityLocation[0].longitude}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
-      );
-
-      const data = await response.json();
-      console.log(data);
-      setWeather(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+export const Day = (props) => {
+  console.log(props.weather);
   return (
-    <div className="border h-180 w-fit p-5 rounded-2xl">
-      <button onClick={getWeather}> weather</button>
-      <h2 className="text-5xl">{weather.name}</h2>
-      <img src="sun.png" className="h-100 w-100" />
-      <h1 className="text-7xl">{weather?.main?.temp}°</h1>
-      <p className="text-orange-400 text-2xl">Bright</p>
+    <div>
+      <div className="border h-180 w-fit p-5 rounded-2xl">
+        <p className="text-amber-50 text-2xl">
+          {props?.weather?.current?.last_updated}
+        </p>
+        <h2 className="text-5xl">{props?.weather?.location?.name}</h2>
+        <img src="sun.png" className="h-100 w-100" />
+        <h1 className="text-7xl">{props?.weather?.current?.temp_c}</h1>
+        <p className="text-orange-400 text-2xl">
+          {props?.weather?.current?.condition?.text}
+        </p>
 
-      <div className="flex justify-between px-5">
-        <House />
-        <MapPin />
-        <Heart />
-        <User />
+        <div className="flex justify-between px-5">
+          <House />
+          <MapPin />
+          <Heart />
+          <User />
+        </div>
       </div>
-      <button onClick={getCity}>city </button>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,55 @@
 import { Day } from "@/component/Day";
-import { Hailt } from "@/component/Hailt";
+import { useEffect, useState } from "react";
+import { Search } from "lucide-react";
 import { Night } from "@/component/Night";
 
-import { Search } from "lucide-react";
-import { useState } from "react";
-export default function Home(){
+export default function Home() {
+  const [weather, setWeather] = useState({});
+  const [searchCity, setSearchCity] = useState("Ulaanbaatar");
+
+  const getWeather = async () => {
+    try {
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${searchCity}`
+      );
+
+      const data = await response.json();
+
+      setWeather(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // useEffect(()=>{
+  //   getWeather ()
+  // })
+ 
+
+
   return (
     <div>
-      <Hailt />
-
+      <div className="flex p-5 relative">
+        <Search className="mt-3" />
+        <input
+          type="search"
+          placeholder="Search"
+          value={searchCity}
+          onChange={(event) => {
+            const value = event.target.value;
+            setSearchCity(value);
+          }}
+          className=" border-0 h-10 w-120 rounded-full bg-amber-50 "
+        ></input>
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          onClick={getWeather}
+        >
+          Search
+        </button>
+      </div>
       <div className="flex w-full gap-0.5">
-        <Day />
-        <Night />
+        <Day weather={weather} />
+        <Night weather={weather} />
       </div>
     </div>
   );
